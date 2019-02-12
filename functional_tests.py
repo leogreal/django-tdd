082f089be63ac2ade1a3f9e256ffe7a97b1dca46
Leonardo Gregório de Almeida <leogreal@gmail.com>
2019-02-12 21:38:50 -0200

    FT agora checa se nós podemos inserir uma tarefa

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -18,31 +20,47 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         """
-        Ela percebe que o título da página e o cabeçãlho mencionam
+        Ela percebe que o título da página e o cabeçalho mencionam
         lista de tarefas (to-do)
         """
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         """
         Ela é convidada a inserir um item de tarefa imediatamente
         """
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         """
         Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma
         caixa de texto. (O hobby de Edith é fazer iscas para pesca com fly)
         """
+        inputbox.send_keys('Buy peacock feathers')
 
         """
         Quando ela tecla enter, a página é atualizada, e agoa a página lista
         '1: Buy peacock feathers' como um item em uma lista de tarefas
         """
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         """
         Ainda continua havendo uma caixa de texto convidando-a acrescentar outro
         item. Ela insere 'Use peacock feathers to make a fly' (Usar penas de pavão
         para fazer um fly - Edith é em metódica)
         """
+        self.fail('Finish the test!')
 
         """
         A página é atualizada novamente e agora mostra os dois itens em sua lista.
@@ -61,7 +79,7 @@ class NewVisitorTest(unittest.TestCase):
         """
         Satisfeita ela volta a dormir
         """
-        
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
